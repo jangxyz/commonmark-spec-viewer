1a773a0efcae4a387d58e2b0e2ae91ee155913b0
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2023-05-02 18:31:10 +0900

    DELETE default sveltekit page

--- src/routes/spec/+page.svelte
+++ src/routes/spec/+page.svelte
@@ -2,17 +2,12 @@
 	import { page } from '$app/stores';
 	import SpecViewer from './SpecViewer.svelte';
 
-	let searchParams = $page.url.searchParams;
-	let urlHash = $page.url.hash;
-	$: searchParams = $page.url.searchParams;
-	$: urlHash = $page.url.hash;
-
 	/** @type {import('./$types').PageData} */
 	export let data;
 </script>
 
 <SpecViewer
 	spec={data.spec}
-	{urlHash}
-	showOnlySelected={searchParams?.get('show-only-selected') === '1'}
+	urlHash={$page.url.hash}
+	showOnlySelected={$page.url.searchParams?.get('show-only-selected') === '1'}
 />
